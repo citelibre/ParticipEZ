adc04e620e56e0f27ed5ce275098b88164c45d02
sbelondr <sbelondr@protonmail.com>
2025-02-13 15:43:33 +0100

    upgrade keycloak, clean arch and add https manage

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -5,8 +5,43 @@ lang_default="${LUTECE_DEFAULT_LANG:-en}"
 
 if [[ "$lang_default" = "fr" ]] ; then 
 	rm /docker-entrypoint-initdb.d/1-dump.sql
+	mv /docker-entrypoint-initdb.d/1-dump.fr.sql /docker-entrypoint-initdb.d/1-dump.sql
 else 
 	rm /docker-entrypoint-initdb.d/1-dump.fr.sql
 fi
+
+protocol=${PROTOCOL}
+
+url_citelibre="${protocol}:\/\/${URL_CITELIBRE}"
+url_matomo_http=${URL_MATOMO_HTTP}
+url_kibana=${URL_KIBANA}
+url_solr=${URL_SOLR}
+url_mail=${URL_MAIL}
+url_keycloak="${protocol}:\/\/${URL_KEYCLOAK}"
+
+sed -i "s/http:\/\/localhost\//${url_matomo_http}\//g" /docker-entrypoint-initdb.d/1-dump.sql
+sed -i "s/http:\/\/localhost\//${url_matomo_http}\//g" /docker-entrypoint-initdb.d/2-dump-matomo.sql
+sed -i "s/http:\/\/localhost\//${url_matomo_http}\//g" /docker-entrypoint-initdb.d/3-dump-keycloak.sql
+
+sed -i "s/http:\/\/localhost:8080/$url_citelibre/g" /docker-entrypoint-initdb.d/1-dump.sql
+sed -i "s/http:\/\/localhost:8080/$url_citelibre/g" /docker-entrypoint-initdb.d/2-dump-matomo.sql
+sed -i "s/http:\/\/localhost:8080/$url_citelibre/g" /docker-entrypoint-initdb.d/3-dump-keycloak.sql
+
+sed -i "s/http:\/\/localhost:5601/$url_kibana/g" /docker-entrypoint-initdb.d/1-dump.sql
+sed -i "s/http:\/\/localhost:5601/$url_kibana/g" /docker-entrypoint-initdb.d/2-dump-matomo.sql
+sed -i "s/http:\/\/localhost:5601/$url_kibana/g" /docker-entrypoint-initdb.d/3-dump-keycloak.sql
+
+sed -i "s/http:\/\/localhost:8081/$url_keycloak/g" /docker-entrypoint-initdb.d/1-dump.sql
+sed -i "s/http:\/\/localhost:8081/$url_keycloak/g" /docker-entrypoint-initdb.d/2-dump-matomo.sql
+sed -i "s/http:\/\/localhost:8081/$url_keycloak/g" /docker-entrypoint-initdb.d/3-dump-keycloak.sql
+
+sed -i "s/http:\/\/localhost:1080/$url_mail/g" /docker-entrypoint-initdb.d/1-dump.sql
+sed -i "s/http:\/\/localhost:1080/$url_mail/g" /docker-entrypoint-initdb.d/2-dump-matomo.sql
+sed -i "s/http:\/\/localhost:1080/$url_mail/g" /docker-entrypoint-initdb.d/3-dump-keycloak.sql
+
+sed -i "s/http:\/\/localhost:8983/$url_mail/g" /docker-entrypoint-initdb.d/1-dump.sql
+sed -i "s/http:\/\/localhost:8983/$url_mail/g" /docker-entrypoint-initdb.d/2-dump-matomo.sql
+sed -i "s/http:\/\/localhost:8983/$url_mail/g" /docker-entrypoint-initdb.d/3-dump-keycloak.sql
+
 
 /usr/local/bin/docker-entrypoint.sh mariadbd
